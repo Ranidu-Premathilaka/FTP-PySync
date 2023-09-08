7c54b6435024ef2757ae1f223ac5804fb68abcb2
Ranidu Lakshan <114408219+Ranidu-Premathilaka@users.noreply.github.com>
2023-09-08 13:07:00 +0530

    delete files if exist

--- ftpSync.py
+++ ftpSync.py
@@ -7,23 +7,34 @@ while(True):
 
     argParser = argparse.ArgumentParser()
 
-    argParser.add_argument("-r", "--root",required=True, help="the root of the folder in this exact format no starting commas /wamp64/www/public ")
-    argParser.add_argument("-o", "--outfile",required=False,default='previousMD.json',help="the name of the output file the default is 'previousMD.json'")
-
+    argParser.add_argument("-r", "--root",required=True, help="The root of the folder in this exact format no starting commas /wamp64/www/public ")
+    argParser.add_argument("-o", "--outfile",required=False,default='previousMD.json',help="The name of the output file the default is 'previousMD.json'")
+    argParser.add_argument("-t", "--time",required=False,default=10,help="The time gap between each check if a file is edited")
+    argParser.add_argument("-H", "--host",required=True, help="The host name of the FTP server ex: ftpupload.net")
+    argParser.add_argument("-u", "--user",required=True, help="The username of the FTP account")
+    argParser.add_argument("-p", "--password",required=True, help="The password of the FTP account")
+    argParser.add_argument("-d", "--directory",required=True, help="The directory of the FTP server the files should be coppied. ex: 'htdocs/test'")    
+    
     args = argParser.parse_args()
 
     root = args.root
-    outfile=args.outfile
-
+    outfile = args.outfile
+    time = args.time
+    host = args.host 
+    user = args.user
+    password = args.password
+    directory = args.directory
      
      
     print(root)
     print(outfile)
+    print("press ctrl + c to quit")
 
 
     path= os.path.join(root,"targetdirectory")
 
     mydic = {}
+    delList = {}
     ftpList = []
     fileExist = False
     global ftp
@@ -49,19 +60,26 @@ while(True):
             try:
                 if(mydic[key] == previousMD[key]):
                     continue
-                else:
+                else:              
                     ftpList.append(key)
+                    
+                del previousMD[key]
+                
             except KeyError:
                 ftpList.append(key)
                 pass
+        
+        delList=previousMD;
+        
+        
                 
     else:
         ftpList = list(mydic.keys())
 
 
-    ftp = FTP('ftpupload.net','epiz_34296651','9AESNB4wG4b0')
+    ftp = FTP(host,user,password)
 
-    ftp.cwd('htdocs')
+    ftp.cwd(directory)
 
 
     for key in ftpList:
@@ -78,8 +96,40 @@ while(True):
         print(strippedKey[-1],store)
         
         file.close()
-        ftp.cwd('/htdocs')
+        ftp.cwd(directory)
 
+    for key in delList:
+        strippedKey=key.split(root+"\\",1)[1]
+        ftp.delete(strippedKey)
+    
+    
+        """
+        strippedKey=key.split(root+"\\",1)[1].split("\\")
+        ftp.cwd("/".join(strippedKey[:-1]))
+        ftp.delete(strippedKey[-1])
+       
+        if not ftp.nlst(){
+            
+        }
+        
+        for i in strippedKey[1::-1]:
+        
+            try:
+                ftp.mkd(i)
+            except:
+                pass
+            ftp.cwd(i)
+        file = open(key,'rb')
+        store = ftp.storbinary('STOR {}'.format(strippedKey[-1]), file)
+        print(strippedKey[-1],store)
+        
+        file.close()
+        ftp.cwd(directory)
+        """
+        
+    
+    
+    
     ftp.close()
 
     with open(outfile, "w") as out:
